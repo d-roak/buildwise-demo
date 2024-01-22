eb6e892c94a0778d1b43d4cb6997d0c6a9195cb8
droak <me+git@droak.sh>
2024-01-22 22:44:58 +0100

    add more nums

--- src/Counter.sol
+++ src/Counter.sol
@@ -5,6 +5,11 @@ contract Counter {
     uint256 public number;
     uint128 public number3;
     uint128 public number2;
+    uint128 public number4;
+    uint256 public number5;
+    uint128 public number6;
+    uint128 public number7;
+    uint256 public number8;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
