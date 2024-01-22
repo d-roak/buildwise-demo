f5b6f0fbef7292e2b5002db1621d39d356e65ef8
Oak <5263301+d-roak@users.noreply.github.com>
2024-01-22 21:40:41 +0000

    Update src/Counter.sol
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint128 public number3;
     uint256 public number;
+    uint128 public number3;
     uint128 public number2;
 
     function setNumber(uint256 newNumber) public {
